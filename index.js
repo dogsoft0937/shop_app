fd15b2f1db8587552b4d68e92f71e67c6929d21f
dogsoft0937 <dogsoft0937@khu.ac.kr>
2023-06-28 14:19:30 +0900

    connect mysql

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express=require('express');
 const app=express();
-
+const db=require('./config/db.js');
+db.connect();
 app.listen(80,()=>{
     console.log("hello");
+    db.query('SELECT * from user',(err,rows,fields)=>{
+        if(err) throw err;
+        console.log(rows);
+    })
 })
